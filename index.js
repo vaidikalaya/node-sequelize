0e7dc47e1e287b0a1239f3aa0fab1767794f0227
vaidikalaya <vaidikalaya@gmail.com>
2024-08-15 20:18:32 +0530

    getter, setters and virtuals created

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ app.get('/',function(req,res){
 })
 
 app.get('/employees',empCtrl.getEmployees)
-app.get('/employees/:id',empCtrl.getEmployee)
 app.post('/add_employee',empCtrl.addEmployee)
-app.patch('/update_employee/:id',empCtrl.updateEmployee)
-app.delete('/delete_employee/:id',empCtrl.deleteEmployee)
-
 
 app.listen(3000,()=>{
     console.log('App will run on: http://localhost:3000')
