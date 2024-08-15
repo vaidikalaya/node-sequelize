2abec2b45b4f4752c4f6ca2bfb371c31bfea753d
vaidikalaya <vaidikalaya@gmail.com>
2024-08-15 21:10:39 +0530

    add validation in models

--- models/Employee.js
+++ models/Employee.js
@@ -8,37 +8,26 @@ module.exports = (sequelize,DataTypes,Model) => {
         first_name: {
           type: DataTypes.STRING,
           allowNull: false,
-          //getter
-          get(){
-            const firstName=this.getDataValue('first_name');
-            return firstName?firstName.toUpperCase():null;
+          validate:{
+            isAlpha:true
           }
         },
         last_name: {
           type:DataTypes.STRING,
-          //getter
-          get(){
-            const lastName=this.getDataValue('last_name');
-            return lastName?lastName.toUpperCase():null;
+          validate:{
+            isAlpha:true
           }
         },
-        email: DataTypes.STRING,
+        email: {
+          type:DataTypes.STRING,
+          validate:{
+            isEmail:true
+          }
+        },
         salary: DataTypes.STRING,
         password: {
           type:DataTypes.STRING,
-          //setter
-          set(value) {
-            const gzippedBuffer = gzipSync(value);
-            this.setDataValue('password', gzippedBuffer.toString('base64'));
-          },
         },
-        //virtuals
-        full_name:{
-          type: DataTypes.VIRTUAL,
-          get() {
-            return `${this.first_name} ${this.last_name}`;
-          },
-        }
     },
     {
       sequelize,
