7fabf3057e0842ea3980c8a4fd22bd8bbae12d5d
vaidikalaya <vaidikalaya@gmail.com>
2024-08-18 16:45:03 +0530

    paranoid soft deletion completed

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -4,20 +4,55 @@ const Employee=db.employee;
 const Skill=db.skill;
 
 const getEmployees = async (req,res) => {
+    const resEmp=await Employee.findAll();
+    res.status(200).json(resEmp)
+}
+
+const getEmployeesWithDeleted = async (req,res) => {
     const resEmp=await Employee.findAll({
-        include: Skill,
+        paranoid: false,
     });
     res.status(200).json(resEmp)
 }
 
-const getSkills = async (req,res) => {
-    const resEmp=await Skill.findAll({
-        include: Employee,
+const deleteEmployee = async (req,res) => {
+    const resEmp=await Employee.destroy({
+        where: {
+          id: 2,
+        },
     });
+    res.status(200).json(resEmp)
+}
+
+const forceDeleteEmployee = async (req,res) => {
+    const resEmp=await Employee.destroy({
+        where: {
+          id: 1,
+        },
+        force: true,
+    });
+    res.status(200).json(resEmp)
+}
+
+const restoreEmployee = async (req,res) => {
+    const resEmp=await Employee.restore({
+        where: {
+          id: 2,
+        },
+    });
+    res.status(200).json(resEmp)
+}
+
+const restoreAllEmployee = async (req,res) => {
+    const resEmp=await Employee.restore();
     res.status(200).json(resEmp)
 }
 
 module.exports={
     getEmployees,
-    getSkills
+    getEmployeesWithDeleted,
+    deleteEmployee,
+    forceDeleteEmployee,
+    restoreEmployee,
+    restoreAllEmployee
 }
